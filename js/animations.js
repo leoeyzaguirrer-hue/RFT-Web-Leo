e13c65fefc7ba36f6799f41f3aaf683b7d24cebf
leoeyzaguirrer-hue <leo.eyzaguirrer@gmail.com>
2025-11-28 14:42:36 -0400

    Update animations.js

--- js/animations.js
+++ js/animations.js
@@ -1,1 +1,10 @@
+// animations.js
+function initAnimations() {
+    console.log("Animaciones activadas");
+
+    const elements = document.querySelectorAll("[data-animate]");
+    elements.forEach(el => {
+        el.classList.add("animate-" + el.dataset.animate);
+    });
+}
 
