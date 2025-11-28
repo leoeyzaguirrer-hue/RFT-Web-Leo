8c9b5c36bed7094737184fc7f6305cf63ce81409
leoeyzaguirrer-hue <leo.eyzaguirrer@gmail.com>
2025-11-28 14:42:23 -0400

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,13 @@
+// ui.js
+function initUI() {
+    console.log("UI inicializada");
+
+    // Delegación para botones de navegación
+    document.body.addEventListener("click", (e) => {
+        if (e.target.matches("[data-page]")) {
+            const page = e.target.getAttribute("data-page");
+            loadPage(page);
+        }
+    });
+}
 
